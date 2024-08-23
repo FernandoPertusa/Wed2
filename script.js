c67f862134e156050185167a8194c4d0e4de73bd
FernandoPertusa <fernandopertusa@gmail.com>
2024-08-23 19:58:16 +0200

    initial

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-document.getElementById('miFormulario').addEventListener('submit', function(event) {
+document.getElementById("formulario").addEventListener("submit", function(event) {
     event.preventDefault();
 
-    const nombre = document.getElementById('nombre').value;
-    const apellido = document.getElementById('apellido').value;
-    const anioNacimiento = document.getElementById('anioNacimiento').value;
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const ano = document.getElementById("ano").value;
+    const mensaje = document.getElementById("mensaje");
 
-    const mensaje = document.getElementById('mensaje');
-
-    if (nombre === '' || apellido === '' || anioNacimiento === '') {
-        mensaje.textContent = 'Parece que te has dejado algún campo, papá';
+    if (nombre === "" || apellido === "" || ano === "") {
+        mensaje.textContent = "Parece que te has dejado algún campo, papá";
     } else {
-        mensaje.textContent = 'Muchas gracias, papá!';
-        
-        const tablaRegistros = document.getElementById('tablaRegistros').querySelector('tbody');
-        const nuevaFila = tablaRegistros.insertRow();
-        
-        const celdaNombre = nuevaFila.insertCell(0);
-        const celdaApellido = nuevaFila.insertCell(1);
-        const celdaAnioNacimiento = nuevaFila.insertCell(2);
-        
-        celdaNombre.textContent = nombre;
-        celdaApellido.textContent = apellido;
-        celdaAnioNacimiento.textContent = anioNacimiento;
-
-        document.getElementById('miFormulario').reset();
+        mensaje.textContent = "Muchas gracias, papá!";
+        agregarRegistro(nombre, apellido, ano);
+        document.getElementById("formulario").reset();
     }
 });
+
+function agregarRegistro(nombre, apellido, ano) {
+    const tabla = document.getElementById("tabla-registros").getElementsByTagName("tbody")[0];
+    const nuevaFila = tabla.insertRow();
+
+    const celdaNombre = nuevaFila.insertCell(0);
+    const celdaApellido = nuevaFila.insertCell(1);
+    const celdaAno = nuevaFila.insertCell(2);
+
+    celdaNombre.textContent = nombre;
+    celdaApellido.textContent = apellido;
+    celdaAno.textContent = ano;
+}
